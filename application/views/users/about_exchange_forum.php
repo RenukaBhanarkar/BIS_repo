23d54804ba3ebe3b25f12b9a10e6b7469a3ade18
AkshaySshende <Akshayshendebca1@gmail.com>
2023-06-16 11:06:12 +0530

    changes

--- application/views/users/about_exchange_forum.php
+++ application/views/users/about_exchange_forum.php
@@ -3,7 +3,7 @@
     <div class="col-sx-12 col-sm-12 col-md-12" style="border-left: 3px solid cadetblue; padding: 0px 25px;">
         <div class="static-content">
             <div class="bloginfo">
-                <h3 style="margin-bottom: 0px;margin-top:20px;color: #0086b2!important;font-weight: 600;">About Exchange Forum</h3>
+                <h3 style="margin-bottom: 0px;margin-top:20px;color: #0086b2!important;font-weight: 600;">About Manak Rath</h3>
             </div>
             <div class="heading-underline" style="width: 200px;">
                 <div class="left"></div><div class="right"></div>
